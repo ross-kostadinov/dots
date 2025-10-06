22cad1fdd6af64d4f5591031e13998f4ee6d0259
Ross Kostadinov <ross@ross-mac.local>
2025-10-07 00:01:18 +0300

    Removing gopls from LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -162,7 +162,7 @@ require("lazy").setup({
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls", "ts_ls", "pyright", "gopls", "bashls", "jsonls", "yamlls",
+          "lua_ls", "ts_ls", "pyright", "bashls", "jsonls", "yamlls",
         },
         handlers = {
           -- default handler for installed servers
